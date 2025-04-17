c303454d05822a53da6f93a640cb25e7527569a2
sbejaoui <souheil.bejaoui@acsone.eu>
2025-04-17 12:26:19 +0200

    [REF] split contract module into: contract, contract_line_successor, contract_termination

--- contract/models/__init__.py
+++ contract/models/__init__.py
@@ -9,6 +9,3 @@ from . import contract_modification
 from . import account_move
 from . import res_partner
 from . import contract_tag
-from . import res_company
-from . import res_config_settings
-from . import contract_terminate_reason
