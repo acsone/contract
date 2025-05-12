bbb79ef4babd98faf8a9143cf3edab1d32069a72
Bilel Jouini <bilel.jouini@acsone.eu>
2025-05-12 12:09:12 +0100

    [MIG] contract_sale_payment_mode: Migration to 18.0
    [MIG] contract_sale_payment_mode
    
    [IMP] : pre-commit auto fixes
    
    [FIX] contract_sale_payment_mode
    
    reverting the field payment_mode_id changement and
    replaced onchange_partner_id by _onchange_partner_id in sale order

--- contract_sale_payment_mode/models/sale_order.py
+++ contract_sale_payment_mode/models/sale_order.py
@@ -6,38 +6,35 @@ from odoo import api, fields, models
 
 
 class SaleOrder(models.Model):
-    _inherit = 'sale.order'
+    _inherit = "sale.order"
 
     contract_payment_mode_id = fields.Many2one(
-        comodel_name='account.payment.mode',
-        string='Contract Payment Mode',
-        domain=[('payment_type', '=', 'inbound')],
+        comodel_name="account.payment.mode",
+        string="Contract Payment Mode",
+        domain=[("payment_type", "=", "inbound")],
         index=True,
     )
     specific_contract_payment_mode = fields.Boolean(
         related="company_id.specific_contract_payment_mode",
-        string="Different payment mode for contracts generated from sale "
-               "orders",
+        string="Different payment mode for contracts generated from sale " "orders",
     )
 
-    @api.onchange('partner_id')
-    def onchange_partner_id(self):
-        res = super().onchange_partner_id()
+    @api.onchange("partner_id")
+    def _onchange_partner_id(self):
+        res = super()._onchange_partner_id()
         if self.partner_id:
-            self.contract_payment_mode_id = self.partner_id.with_context(
-                force_company=self.company_id.id
+            self.contract_payment_mode_id = self.partner_id.with_company(
+                self.company_id
             ).customer_payment_mode_id
         else:
             self.payment_mode_id = False
         return res
 
-    @api.multi
     def _prepare_contract_value(self, contract_template):
         self.ensure_one()
-        vals = super(SaleOrder, self)._prepare_contract_value(
-            contract_template)
+        vals = super()._prepare_contract_value(contract_template)
         if self.specific_contract_payment_mode:
-            vals['payment_mode_id'] = self.contract_payment_mode_id.id
+            vals["payment_mode_id"] = self.contract_payment_mode_id.id
         else:
-            vals['payment_mode_id'] = self.payment_mode_id.id
+            vals["payment_mode_id"] = self.payment_mode_id.id
         return vals
