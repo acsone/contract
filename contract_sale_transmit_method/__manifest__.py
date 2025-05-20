e760371f4ff9bd6fbacf957c470b8dc35ab427ae
sbejaoui <souheil.bejaoui@acsone.eu>
2025-05-20 17:31:46 +0200

    [MIG] contract_sale_transmit_method: Migration to 18.0

--- contract_sale_transmit_method/__manifest__.py
+++ contract_sale_transmit_method/__manifest__.py
@@ -6,10 +6,13 @@
     "summary": """
         Propagate transmit method (email, post, portal, ...) from sale orders
         to contracts.""",
-    "version": "12.0.1.0.0",
+    "version": "18.0.1.0.0",
     "license": "AGPL-3",
-    "author": "ACSONE SA/NV,"
-              "Odoo Community Association (OCA)",
+    "author": "ACSONE SA/NV," "Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/contract",
-    "depends": ["sale_order_transmit_method", "contract_transmit_method"],
+    "depends": [
+        "sale_order_transmit_method",
+        "contract_transmit_method",
+        "product_contract",
+    ],
 }
