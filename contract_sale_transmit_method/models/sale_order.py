146fabd4f5ce494072ceacebd6bcbf8fba0533b1
sbejaoui <souheil.bejaoui@acsone.eu>
2025-05-20 17:31:46 +0200

    [MIG] contract_sale_transmit_method: Migration to 18.0

--- contract_sale_transmit_method/models/sale_order.py
+++ contract_sale_transmit_method/models/sale_order.py
@@ -1,14 +1,12 @@
 # Copyright 2020 ACSONE SA/NV
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import api, models
+from odoo import models
 
 
 class SaleOrder(models.Model):
-
     _inherit = "sale.order"
 
-    @api.multi
     def _prepare_contract_value(self, contract_template):
         contract_values = super()._prepare_contract_value(contract_template)
         contract_values["transmit_method_id"] = self.transmit_method_id.id
