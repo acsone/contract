dcbc31ebf27a8dc92ef455922cfa0c6ceaabea5f
Raphael Vicini <raphael.vicini@acsone.eu>
2026-08-26 11:44:51 +0200

    [MIG] contract_sale_delivery_zone: Migration to 19.0

--- contract_sale_delivery_zone/models/sale_order.py
+++ contract_sale_delivery_zone/models/sale_order.py
@@ -2,7 +2,6 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 from odoo import api, models
-from odoo.fields import first
 
 
 class SaleOrder(models.Model):
@@ -18,11 +17,10 @@ class SaleOrder(models.Model):
             )
         )
         for sale in contract_sales:
-            delivery_zone = first(
-                sale.mapped(
-                    "order_line.contract_line_id.contract_id.partner_delivery_zone_id"
-                )
+            delivery_zone = sale.mapped(
+                "order_line.contract_line_id.contract_id.partner_delivery_zone_id"
             )
-            sale.delivery_zone_id = delivery_zone
+
+            sale.delivery_zone_id = delivery_zone[:1]
         # Fallback to normal sale orders
         return super(SaleOrder, (self - contract_sales))._compute_delivery_zone_id()
